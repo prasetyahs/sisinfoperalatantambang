542f1e76801c5c412f0bde9ed3f920f09e3adada
prasetya2423@gmail.com <prasetya2423@gmail.com>
2019-11-27 07:51:32 +0700

    kocak

--- application/views/layout/home/footer.php
+++ application/views/layout/home/footer.php
@@ -9,14 +9,11 @@
         </div>
         <div class="row">
             <div class="col-md-12 text-center">
-
                 <p>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     Copyright &copy;
                     <script>
                         document.write(new Date().getFullYear());
                     </script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="" style="color:#fff " target="_blank">PT NESITOR INDONESIA</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
             </div>
         </div>
